f3ea0754d306d07c736fb7daaf43688bc526d6fa
Yassir Awad <yassir.awad@dce.sa>
2019-06-20 13:50:03 +0300

    updats

--- src/ForwarderServiceProvider.php
+++ src/ForwarderServiceProvider.php
@@ -10,37 +10,18 @@ class ForwarderServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Route::macro('getFwd', function ($route, $path = null) {
-            Route::get($route, $this->getPath($path))->middleware("forward");
-        });
+        foreach (['get', 'post', 'delete', 'put', 'patch'] as $f) {
+            Route::macro("{$f}Fwd", function ($route, $path = null) use ($f) {
+                if (!$path) {
+                    $path = [SendController::class, 'send'];
+                }
 
-        Route::macro('postFwd', function ($route, $path = null) {
-            Route::post($route, $this->getPath($path))->middleware("forward");
-        });
-
-        Route::macro('putFwd', function ($route, $path = null) {
-            Route::put($route, $this->getPath($path))->middleware("forward");
-        });
-
-        Route::macro('patchFwd', function ($route, $path = null) {
-            Route::patch($route, $this->getPath($path))->middleware("forward");
-        });
-
-        Route::macro('deleteFwd', function ($route, $path = null) {
-            Route::delete($route, $this->getPath($path))->middleware("forward");
-        });
+                Route::$f($route, $path)->middleware("forward");
+            });
+        }
 
         $this->publishes([
             __DIR__ . './config/forward.php' => config_path('forward.php'),
         ], 'config');
-    }
-
-    private function getPath($path)
-    {
-        if (!$path) {
-            $path = [SendController::class, 'send'];
-        }
-
-        return $path;
     }
 }
