782dcc9d14ea02612584c3874671b248b86e618d
Yassir Awad <yassir.awad@dce.sa>
2019-06-20 12:35:22 +0300

    updats

--- src/ForwarderServiceProvider.php
+++ src/ForwarderServiceProvider.php
@@ -10,11 +10,31 @@ class ForwarderServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Route::macro('forward', function ($method, $route) {
+        Route::macro('getFwd', function ($route) {
             $path = [SendController::class, 'send'];
-
-            Route::$method($route, $path)->middleware("forward:$route");
+            Route::get($route, $path)->middleware("forward:$route");
         });
+
+        Route::macro('postFwd', function ($route) {
+            $path = [SendController::class, 'send'];
+            Route::post($route, $path)->middleware("forward:$route");
+        });
+
+        Route::macro('putFwd', function ($route) {
+            $path = [SendController::class, 'send'];
+            Route::put($route, $path)->middleware("forward:$route");
+        });
+
+        Route::macro('patchFwd', function ($route) {
+            $path = [SendController::class, 'send'];
+            Route::patch($route, $path)->middleware("forward:$route");
+        });
+
+        Route::macro('deleteFwd', function ($route) {
+            $path = [SendController::class, 'send'];
+            Route::delete($route, $path)->middleware("forward:$route");
+        });
+
 
         $this->publishes([
             __DIR__ . './config/forward.php' => config_path('forward.php'),
